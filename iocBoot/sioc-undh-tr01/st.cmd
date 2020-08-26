f0ec48fd10aa14669125fe917f5bb381eb2efaca
rreno <rreno@slac.stanford.edu>
2020-08-26 11:14:47 -0700

    Fixed missing BEND_MAG macro in UNDH IOCs discovered when deploying R3.2.2 to prod

--- iocBoot/sioc-undh-tr01/st.cmd
+++ iocBoot/sioc-undh-tr01/st.cmd
@@ -39,6 +39,9 @@ epicsEnvSet("FB", "FB03")
 epicsEnvSet("LOOP", "TR04")
 epicsEnvSet("CONFIG_NAME", "LaunchLoop1")
 
+# Which BY1 bend magnet do we want to read energy from?
+epicsEnvSet("BEND_MAG", "BEND:LTUH:125:BDES")
+
 #===================================================================
 # Set MACROS for EVRs & VIOC
 #===================================================================
