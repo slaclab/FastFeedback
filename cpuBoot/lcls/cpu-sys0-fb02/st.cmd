c49fcc184243eeedbcd02eaa804a285c2c35ad99
Kristi Luchini <luchini@slac.stanford.edu>
2020-09-21 14:43:01 -0700

    CPU Startup script

---
+++ cpuBoot/lcls/cpu-sys0-fb02/st.cmd
@@ -0,0 +1,76 @@
+#==============================================================
+#
+#  Abs:  Startup Script for cpu-sys0-fb02 at B005-3023
+#
+#  Name: st.cmd
+#
+#  Desc: The processor is an Advantech 1U server,
+#        SKY-8100 industrial pc/server.
+#
+#  Facility: LCLS Fasfeedaback Controls
+#
+#  Auth: 27-Aug-2020, Jeremy Mock     (JMOCK)
+#  Rev:  dd-mmm-yyyy, Reviewer's Name (USERNAME)
+#--------------------------------------------------------------
+#  Mod:
+#        21-Sep-2020, K. Luchini      (LUCHINI):
+#         extract from $EPICS_IOCS/cpu-sys-fb02/kernel-module.cmd
+#
+#==============================================================
+
+# =========================================
+# Turn on local IPMI Client support
+# ========================================
+modprobe ipmi_devintf
+ipmitool lan print
+
+# =========================================
+# Timing - evr pci device managment
+# =========================================
+loadModule.sh $EPICS_CPU_LIB/evrma.ko
+chmod 666 /dev/evr0mng
+
+evrManager /dev/evr0mng create vevr0
+evrManager /dev/evr0mng create vevr1
+evrManager /dev/evr0mng create vevr2
+evrManager /dev/evr0mng create vevr3
+evrManager /dev/evr0mng create vevr4
+evrManager /dev/evr0mng create vevr5
+evrManager /dev/evr0mng create vevr6
+evrManager /dev/evr0mng create vevr7
+evrManager /dev/evr0mng create vevr8
+evrManager /dev/evr0mng create vevr9
+
+# wait a bit to device become visible
+sleep 1
+
+chmod 666 /dev/vevr0
+chmod 666 /dev/vevr1
+chmod 666 /dev/vevr2
+chmod 666 /dev/vevr3
+chmod 666 /dev/vevr4
+chmod 666 /dev/vevr5
+chmod 666 /dev/vevr6
+chmod 666 /dev/vevr7
+chmod 666 /dev/vevr8
+chmod 666 /dev/vevr9
+echo "Initializing evrManager"
+evrManager /dev/evr0mng init
+
+# ============================================================================
+# Let's activate another ethernet port:
+# Let's activate the second network chip for Fast Feedback. cpu-sys0-fb01-fnet
+# Name:   cpu-sys0-fb01-fnet.slac.stanford.edu
+# Address: 172.27.29.102
+# ============================================================================
+ifconfig eth2 172.27.29.102 netmask 255.255.252.0
+export FCOMMCGRP="mc-lcls-fcom"
+route add -net $FCOMMCGRP netmask 255.255.252.0 dev eth2
+ip route add 224.0.0.0/4 dev eth2
+export FCOM_MC_PREFIX=239.219.8.0
+
+# End of script
+
+
+
+
