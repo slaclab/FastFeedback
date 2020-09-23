b8ac85d544bf671d7a4d2d9bbaadd354258ae1f4
rreno <rreno@slac.stanford.edu>
2020-09-23 12:53:45 -0700

    Moved PatternMask::destinationHXR documentation comment outside of function body

--- fastFeedbackApp/src/framework/PatternMask.cc
+++ fastFeedbackApp/src/framework/PatternMask.cc
@@ -197,20 +197,19 @@ std::string PatternMask::toString() {
     return str.str();
 }
 
+/**
+ * If the destination is HXR it will either have BKRCUS in its exclusion
+ * mask or BKRCUS will not be in its inclusion mask (or both).
+ *
+ * If a mask contains BKRCUS the bitwise and with the mask and BKRCUS will
+ * be a non-zero (true) value. Likewise, if BKRCUS is not in the mask, the
+ * bitwise and will be zero (false). We can just return the logical or of
+ * these two bitwise operations to determine the destination this pattern
+ * mask is configured for.
+ */
 bool
 PatternMask::destinationHXR()
 {
-    /*
-     * If the destination is HXR it will either have BKRCUS in its exclusion
-     * mask or BKRCUS will not be in its inclusion mask (or both).
-     *
-     * If a mask contains BKRCUS the bitwise and with the mask and BKRCUS will
-     * be a non-zero (true) value. Likewise, if BKRCUS is not in the mask, the
-     * bitwise and will be zero (false). We can just return the logical or of
-     * these two bitwise operations to determine the destination this pattern
-     * mask is configured for.
-     */
-
     return ( _exclusionMask[MOD3_IDX] & BKRCUS ||
            !(_inclusionMask[MOD3_IDX] & BKRCUS) );
 }
