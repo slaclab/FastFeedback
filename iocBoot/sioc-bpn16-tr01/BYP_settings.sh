27a3c213d99f2eb9deea929d7761fbbb4368a833
Jeremy Mock <jeremy.a.mock@gmail.com>
2023-08-24 07:55:31 -0700

    Add IOCs for SC

---
+++ iocBoot/sioc-bpn16-tr01/BYP_settings.sh
@@ -0,0 +1,212 @@
+#!/bin/sh
+# === Feedback Configuration for FB06:TR06 ===
+caput FBCK:FB06:TR06:NAME            "BYP Launch" > /dev/null
+caput FBCK:FB06:TR06:ITERATEFUNC     TrajectoryFitBy1 > /dev/null
+caput FBCK:FB06:TR06:TOTALPOI        1  > /dev/null
+caput FBCK:FB06:TR06:SELECTALLDEVICES NO  > /dev/null
+caput FBCK:FB06:TR06:MEASNUM         12  > /dev/null
+caput FBCK:FB06:TR06:ACTNUM          4  > /dev/null
+caput FBCK:FB06:TR06:STATENUM        4  > /dev/null
+caput FBCK:FB06:TR06:PGAIN           0.03  > /dev/null
+caput FBCK:FB06:TR06:IGAIN           0  > /dev/null
+caput FBCK:FB06:TR06:M1DEVNAME       BPMS:BPN21:400:FW:X
+caput FBCK:FB06:TR06:M2DEVNAME       BPMS:BPN22:400:FW:X
+caput FBCK:FB06:TR06:M3DEVNAME       BPMS:BPN23:400:FW:X
+caput FBCK:FB06:TR06:M4DEVNAME       BPMS:BPN24:400:FW:X
+caput FBCK:FB06:TR06:M5DEVNAME       BPMS:BPN26:400:FW:X
+caput FBCK:FB06:TR06:M6DEVNAME       BPMS:BPN27:400:FW:X
+caput FBCK:FB06:TR06:M7DEVNAME       BPMS:BPN21:400:FW:Y
+caput FBCK:FB06:TR06:M8DEVNAME       BPMS:BPN22:400:FW:Y
+caput FBCK:FB06:TR06:M9DEVNAME       BPMS:BPN23:400:FW:Y
+caput FBCK:FB06:TR06:M10DEVNAME      BPMS:BPN24:400:FW:Y
+caput FBCK:FB06:TR06:M11DEVNAME      BPMS:BPN26:400:FW:Y
+caput FBCK:FB06:TR06:M12DEVNAME      BPMS:BPN27:400:FW:Y
+caput FBCK:FB06:TR06:M13DEVNAME      NULL
+caput FBCK:FB06:TR06:M14DEVNAME      NULL
+caput FBCK:FB06:TR06:M15DEVNAME      NULL
+caput FBCK:FB06:TR06:M16DEVNAME      NULL
+caput FBCK:FB06:TR06:M17DEVNAME      NULL
+caput FBCK:FB06:TR06:M18DEVNAME      NULL
+caput FBCK:FB06:TR06:M19DEVNAME      NULL
+caput FBCK:FB06:TR06:M20DEVNAME      NULL
+caput FBCK:FB06:TR06:M1USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M2USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M3USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M4USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M5USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M6USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M7USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M8USED          YES  > /dev/null
+caput FBCK:FB06:TR06:M9USED          NO  > /dev/null
+caput FBCK:FB06:TR06:M10USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M11USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M12USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M13USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M14USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M15USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M16USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M17USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M18USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M19USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M20USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M1CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M2CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M3CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M4CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M5CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M6CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M7CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M8CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:M9CAMODE        NO  > /dev/null
+caput FBCK:FB06:TR06:M10CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M11CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M12CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M13CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M14CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M15CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M16CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M17CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M18CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M19CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:M20CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR06:A1DEVNAME       XCOR:BPN17:406:BCTRL > /dev/null
+caput FBCK:FB06:TR06:A2DEVNAME       XCOR:BPN21:406:BCTRL > /dev/null
+caput FBCK:FB06:TR06:A3DEVNAME       YCOR:BPN16:409:BCTRL > /dev/null
+caput FBCK:FB06:TR06:A4DEVNAME       YCOR:BPN20:409:BCTRL > /dev/null
+caput FBCK:FB06:TR06:A5DEVNAME       NULL > /dev/null
+caput FBCK:FB06:TR06:A6DEVNAME       NULL > /dev/null
+caput FBCK:FB06:TR06:A7DEVNAME       NULL  > /dev/null
+caput FBCK:FB06:TR06:A8DEVNAME       NULL  > /dev/null
+caput FBCK:FB06:TR06:A9DEVNAME       NULL  > /dev/null
+caput FBCK:FB06:TR06:A10DEVNAME      NULL  > /dev/null
+caput FBCK:FB06:TR06:A1USED          YES  > /dev/null
+caput FBCK:FB06:TR06:A2USED          YES  > /dev/null
+caput FBCK:FB06:TR06:A3USED          YES  > /dev/null
+caput FBCK:FB06:TR06:A4USED          YES  > /dev/null
+caput FBCK:FB06:TR06:A5USED          NO  > /dev/null
+caput FBCK:FB06:TR06:A6USED          NO  > /dev/null
+caput FBCK:FB06:TR06:A7USED          NO  > /dev/null
+caput FBCK:FB06:TR06:A8USED          NO  > /dev/null
+caput FBCK:FB06:TR06:A9USED          NO  > /dev/null
+caput FBCK:FB06:TR06:A10USED         NO  > /dev/null
+caput FBCK:FB06:TR06:A1CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A2CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A3CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A4CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A5CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A6CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A7CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A8CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A9CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR06:A10CAMODE       YES  > /dev/null
+caput FBCK:FB06:TR06:S1NAME          "X.Pos"  > /dev/null
+caput FBCK:FB06:TR06:S2NAME          "X.Ang" > /dev/null
+caput FBCK:FB06:TR06:S3NAME          "Y.Pos"  > /dev/null
+caput FBCK:FB06:TR06:S4NAME          "Y.Ang"  > /dev/null
+caput FBCK:FB06:TR06:S5NAME          ""  > /dev/null
+caput FBCK:FB06:TR06:S6NAME          ""  > /dev/null
+caput FBCK:FB06:TR06:S7NAME          ""  > /dev/null
+caput FBCK:FB06:TR06:S8NAME          ""  > /dev/null
+caput FBCK:FB06:TR06:S9NAME          ""  > /dev/null
+caput FBCK:FB06:TR06:S10NAME         ""  > /dev/null
+caput FBCK:FB06:TR06:S1USED          YES  > /dev/null
+caput FBCK:FB06:TR06:S2USED          YES  > /dev/null
+caput FBCK:FB06:TR06:S3USED          YES  > /dev/null
+caput FBCK:FB06:TR06:S4USED          YES  > /dev/null
+caput FBCK:FB06:TR06:S5USED          NO  > /dev/null
+caput FBCK:FB06:TR06:S6USED          NO  > /dev/null
+caput FBCK:FB06:TR06:S7USED          NO  > /dev/null
+caput FBCK:FB06:TR06:S8USED          NO  > /dev/null
+caput FBCK:FB06:TR06:S9USED          NO  > /dev/null
+caput FBCK:FB06:TR06:S10USED         NO  > /dev/null
+caput FBCK:FB06:TR06:M1HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M2HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M3HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M4HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M5HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M6HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M7HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M8HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M9HIHI          12  > /dev/null
+caput FBCK:FB06:TR06:M10HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M11HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M12HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M13HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M14HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M15HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M16HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M17HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M18HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M19HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M20HIHI         12  > /dev/null
+caput FBCK:FB06:TR06:M1LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M2LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M3LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M4LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M5LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M6LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M7LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M8LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M9LOLO          -12  > /dev/null
+caput FBCK:FB06:TR06:M10LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M11LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M12LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M13LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M14LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M15LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M16LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M17LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M18LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M19LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:M20LOLO         -12  > /dev/null
+caput FBCK:FB06:TR06:A1HIHI          0.0189  > /dev/null
+caput FBCK:FB06:TR06:A2HIHI          0.0189  > /dev/null
+caput FBCK:FB06:TR06:A3HIHI          0.0189  > /dev/null
+caput FBCK:FB06:TR06:A4HIHI          0.0189  > /dev/null
+caput FBCK:FB06:TR06:A5HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR06:A6HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR06:A7HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR06:A8HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR06:A9HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR06:A10HIHI         0.021  > /dev/null
+caput FBCK:FB06:TR06:A1LOLO          -0.0189  > /dev/null
+caput FBCK:FB06:TR06:A2LOLO          -0.0189  > /dev/null
+caput FBCK:FB06:TR06:A3LOLO          -0.0189  > /dev/null
+caput FBCK:FB06:TR06:A4LOLO          -0.0189  > /dev/null
+caput FBCK:FB06:TR06:A5LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR06:A6LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR06:A7LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR06:A8LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR06:A9LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR06:A10LOLO         -0.021  > /dev/null
+caput FBCK:FB06:TR06:S1HIHI          10  > /dev/null
+caput FBCK:FB06:TR06:S2HIHI          10  > /dev/null
+caput FBCK:FB06:TR06:S3HIHI          10  > /dev/null
+caput FBCK:FB06:TR06:S4HIHI          10  > /dev/null
+caput FBCK:FB06:TR06:S5HIHI          3  > /dev/null
+caput FBCK:FB06:TR06:S6HIHI          3  > /dev/null
+caput FBCK:FB06:TR06:S7HIHI          3  > /dev/null
+caput FBCK:FB06:TR06:S8HIHI          3  > /dev/null
+caput FBCK:FB06:TR06:S9HIHI          3  > /dev/null
+caput FBCK:FB06:TR06:S10HIHI         3  > /dev/null
+caput FBCK:FB06:TR06:S1LOLO          -10  > /dev/null
+caput FBCK:FB06:TR06:S2LOLO          -10  > /dev/null
+caput FBCK:FB06:TR06:S3LOLO          -10  > /dev/null
+caput FBCK:FB06:TR06:S4LOLO          -10  > /dev/null
+caput FBCK:FB06:TR06:S5LOLO          -3  > /dev/null
+caput FBCK:FB06:TR06:S6LOLO          -3  > /dev/null
+caput FBCK:FB06:TR06:S7LOLO          -3  > /dev/null
+caput FBCK:FB06:TR06:S8LOLO          -3  > /dev/null
+caput FBCK:FB06:TR06:S9LOLO          -3  > /dev/null
+caput FBCK:FB06:TR06:S10LOLO         -3  > /dev/null
+caput -a FBCK:FB06:TR06:FMATRIX 200 1 -6.19215e-17 0 0 -1.73472e-18 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:GMATRIX 200 8.02303 0 0 8.12022 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:MEASDSPR 40 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:ACTENERGY 10 3.5 3.5 3.5 3.5 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:REFORBIT 40 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:POI1 21 0 0 0 8 0 0 0 32 0 0 55 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:POI2 21 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:POI3 21 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR06:POI4 21 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput FBCK:FB06:TR06:INSTALLED 1 > /dev/null
+echo === Restored configuration for FB06:TR06 ===
---
+++ iocBoot/sioc-diag0-tr01/DIAG0_settings.sh
@@ -0,0 +1,212 @@
+#!/bin/sh
+# === Feedback Configuration for FB06:TR03 ===
+caput FBCK:FB06:TR03:NAME            L3BLaunch > /dev/null
+caput FBCK:FB06:TR03:ITERATEFUNC     TrajectoryFitBy1 > /dev/null
+caput FBCK:FB06:TR03:TOTALPOI        1  > /dev/null
+caput FBCK:FB06:TR03:SELECTALLDEVICES NO  > /dev/null
+caput FBCK:FB06:TR03:MEASNUM         6  > /dev/null
+caput FBCK:FB06:TR03:ACTNUM          4  > /dev/null
+caput FBCK:FB06:TR03:STATENUM        4  > /dev/null
+caput FBCK:FB06:TR03:PGAIN           0.03  > /dev/null
+caput FBCK:FB06:TR03:IGAIN           0  > /dev/null
+caput FBCK:FB06:TR03:M1DEVNAME       BPMS:DIAG0:370:FW:X
+caput FBCK:FB06:TR03:M2DEVNAME       BPMS:DIAG0:390:FW:X
+caput FBCK:FB06:TR03:M3DEVNAME       BPMS:DIAG0:470:FW:X
+caput FBCK:FB06:TR03:M4DEVNAME       BPMS:DIAG0:370:FW:Y
+caput FBCK:FB06:TR03:M5DEVNAME       BPMS:DIAG0:390:FW:Y
+caput FBCK:FB06:TR03:M6DEVNAME       BPMS:DIAG0:470:FW:Y
+caput FBCK:FB06:TR03:M7DEVNAME       NULL
+caput FBCK:FB06:TR03:M8DEVNAME       NULL
+caput FBCK:FB06:TR03:M9DEVNAME       NULL
+caput FBCK:FB06:TR03:M10DEVNAME      NULL
+caput FBCK:FB06:TR03:M11DEVNAME      NULL
+caput FBCK:FB06:TR03:M12DEVNAME      NULL
+caput FBCK:FB06:TR03:M13DEVNAME      NULL
+caput FBCK:FB06:TR03:M14DEVNAME      NULL
+caput FBCK:FB06:TR03:M15DEVNAME      NULL
+caput FBCK:FB06:TR03:M16DEVNAME      NULL
+caput FBCK:FB06:TR03:M17DEVNAME      NULL
+caput FBCK:FB06:TR03:M18DEVNAME      NULL
+caput FBCK:FB06:TR03:M19DEVNAME      NULL
+caput FBCK:FB06:TR03:M20DEVNAME      NULL
+caput FBCK:FB06:TR03:M1USED          YES  > /dev/null
+caput FBCK:FB06:TR03:M2USED          YES  > /dev/null
+caput FBCK:FB06:TR03:M3USED          YES  > /dev/null
+caput FBCK:FB06:TR03:M4USED          YES  > /dev/null
+caput FBCK:FB06:TR03:M5USED          YES  > /dev/null
+caput FBCK:FB06:TR03:M6USED          YES  > /dev/null
+caput FBCK:FB06:TR03:M7USED          NO  > /dev/null
+caput FBCK:FB06:TR03:M8USED          NO  > /dev/null
+caput FBCK:FB06:TR03:M9USED          NO  > /dev/null
+caput FBCK:FB06:TR03:M10USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M11USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M12USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M13USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M14USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M15USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M16USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M17USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M18USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M19USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M20USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M1CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:M2CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:M3CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:M4CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:M5CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:M6CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:M7CAMODE        NO  > /dev/null
+caput FBCK:FB06:TR03:M8CAMODE        NO  > /dev/null
+caput FBCK:FB06:TR03:M9CAMODE        NO  > /dev/null
+caput FBCK:FB06:TR03:M10CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M11CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M12CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M13CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M14CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M15CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M16CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M17CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M18CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M19CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:M20CAMODE       NO  > /dev/null
+caput FBCK:FB06:TR03:A1DEVNAME       XCOR:DIAG0:178:BCTRL > /dev/null
+caput FBCK:FB06:TR03:A2DEVNAME       XCOR:DIAG0:290:BCTRL > /dev/null
+caput FBCK:FB06:TR03:A3DEVNAME       YCOR:DIAG0:199:BCTRL > /dev/null
+caput FBCK:FB06:TR03:A4DEVNAME       YCOR:DIAG0:290:BCTRL > /dev/null
+caput FBCK:FB06:TR03:A5DEVNAME       NULL > /dev/null
+caput FBCK:FB06:TR03:A6DEVNAME       NULL > /dev/null
+caput FBCK:FB06:TR03:A7DEVNAME       NULL  > /dev/null
+caput FBCK:FB06:TR03:A8DEVNAME       NULL  > /dev/null
+caput FBCK:FB06:TR03:A9DEVNAME       NULL  > /dev/null
+caput FBCK:FB06:TR03:A10DEVNAME      NULL  > /dev/null
+caput FBCK:FB06:TR03:A1USED          YES  > /dev/null
+caput FBCK:FB06:TR03:A2USED          YES  > /dev/null
+caput FBCK:FB06:TR03:A3USED          YES  > /dev/null
+caput FBCK:FB06:TR03:A4USED          YES  > /dev/null
+caput FBCK:FB06:TR03:A5USED          NO  > /dev/null
+caput FBCK:FB06:TR03:A6USED          NO  > /dev/null
+caput FBCK:FB06:TR03:A7USED          NO  > /dev/null
+caput FBCK:FB06:TR03:A8USED          NO  > /dev/null
+caput FBCK:FB06:TR03:A9USED          NO  > /dev/null
+caput FBCK:FB06:TR03:A10USED         NO  > /dev/null
+caput FBCK:FB06:TR03:A1CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A2CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A3CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A4CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A5CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A6CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A7CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A8CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A9CAMODE        YES  > /dev/null
+caput FBCK:FB06:TR03:A10CAMODE       YES  > /dev/null
+caput FBCK:FB06:TR03:S1NAME          "X.Pos"  > /dev/null
+caput FBCK:FB06:TR03:S2NAME          "X.Ang" > /dev/null
+caput FBCK:FB06:TR03:S3NAME          "Y.Pos"  > /dev/null
+caput FBCK:FB06:TR03:S4NAME          "Y.Ang"  > /dev/null
+caput FBCK:FB06:TR03:S5NAME          ""  > /dev/null
+caput FBCK:FB06:TR03:S6NAME          ""  > /dev/null
+caput FBCK:FB06:TR03:S7NAME          ""  > /dev/null
+caput FBCK:FB06:TR03:S8NAME          ""  > /dev/null
+caput FBCK:FB06:TR03:S9NAME          ""  > /dev/null
+caput FBCK:FB06:TR03:S10NAME         ""  > /dev/null
+caput FBCK:FB06:TR03:S1USED          YES  > /dev/null
+caput FBCK:FB06:TR03:S2USED          YES  > /dev/null
+caput FBCK:FB06:TR03:S3USED          YES  > /dev/null
+caput FBCK:FB06:TR03:S4USED          YES  > /dev/null
+caput FBCK:FB06:TR03:S5USED          NO  > /dev/null
+caput FBCK:FB06:TR03:S6USED          NO  > /dev/null
+caput FBCK:FB06:TR03:S7USED          NO  > /dev/null
+caput FBCK:FB06:TR03:S8USED          NO  > /dev/null
+caput FBCK:FB06:TR03:S9USED          NO  > /dev/null
+caput FBCK:FB06:TR03:S10USED         NO  > /dev/null
+caput FBCK:FB06:TR03:M1HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M2HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M3HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M4HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M5HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M6HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M7HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M8HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M9HIHI          12  > /dev/null
+caput FBCK:FB06:TR03:M10HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M11HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M12HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M13HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M14HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M15HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M16HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M17HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M18HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M19HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M20HIHI         12  > /dev/null
+caput FBCK:FB06:TR03:M1LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M2LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M3LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M4LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M5LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M6LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M7LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M8LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M9LOLO          -12  > /dev/null
+caput FBCK:FB06:TR03:M10LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M11LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M12LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M13LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M14LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M15LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M16LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M17LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M18LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M19LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:M20LOLO         -12  > /dev/null
+caput FBCK:FB06:TR03:A1HIHI          0.0122  > /dev/null
+caput FBCK:FB06:TR03:A2HIHI          0.0049  > /dev/null
+caput FBCK:FB06:TR03:A3HIHI          0.0122  > /dev/null
+caput FBCK:FB06:TR03:A4HIHI          0.0049  > /dev/null
+caput FBCK:FB06:TR03:A5HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR03:A6HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR03:A7HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR03:A8HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR03:A9HIHI          0.021  > /dev/null
+caput FBCK:FB06:TR03:A10HIHI         0.021  > /dev/null
+caput FBCK:FB06:TR03:A1LOLO          -0.0122  > /dev/null
+caput FBCK:FB06:TR03:A2LOLO          -0.0049  > /dev/null
+caput FBCK:FB06:TR03:A3LOLO          -0.0122  > /dev/null
+caput FBCK:FB06:TR03:A4LOLO          -0.0049  > /dev/null
+caput FBCK:FB06:TR03:A5LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR03:A6LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR03:A7LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR03:A8LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR03:A9LOLO          -0.021  > /dev/null
+caput FBCK:FB06:TR03:A10LOLO         -0.021  > /dev/null
+caput FBCK:FB06:TR03:S1HIHI          10  > /dev/null
+caput FBCK:FB06:TR03:S2HIHI          10  > /dev/null
+caput FBCK:FB06:TR03:S3HIHI          10  > /dev/null
+caput FBCK:FB06:TR03:S4HIHI          10  > /dev/null
+caput FBCK:FB06:TR03:S5HIHI          3  > /dev/null
+caput FBCK:FB06:TR03:S6HIHI          3  > /dev/null
+caput FBCK:FB06:TR03:S7HIHI          3  > /dev/null
+caput FBCK:FB06:TR03:S8HIHI          3  > /dev/null
+caput FBCK:FB06:TR03:S9HIHI          3  > /dev/null
+caput FBCK:FB06:TR03:S10HIHI         3  > /dev/null
+caput FBCK:FB06:TR03:S1LOLO          -10  > /dev/null
+caput FBCK:FB06:TR03:S2LOLO          -10  > /dev/null
+caput FBCK:FB06:TR03:S3LOLO          -10  > /dev/null
+caput FBCK:FB06:TR03:S4LOLO          -10  > /dev/null
+caput FBCK:FB06:TR03:S5LOLO          -3  > /dev/null
+caput FBCK:FB06:TR03:S6LOLO          -3  > /dev/null
+caput FBCK:FB06:TR03:S7LOLO          -3  > /dev/null
+caput FBCK:FB06:TR03:S8LOLO          -3  > /dev/null
+caput FBCK:FB06:TR03:S9LOLO          -3  > /dev/null
+caput FBCK:FB06:TR03:S10LOLO         -3  > /dev/null
+caput -a FBCK:FB06:TR03:FMATRIX 200 1 -6.19215e-17 0 0 -1.73472e-18 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:GMATRIX 200 8.02303 0 0 8.12022 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:MEASDSPR 40 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:ACTENERGY 10 0.08 0.08 0.08 0.08 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:REFORBIT 40 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:POI1 21 0 0 0 8 0 0 0 32 0 0 55 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:POI2 21 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:POI3 21 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput -a FBCK:FB06:TR03:POI4 21 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0  > /dev/null
+caput FBCK:FB06:TR03:INSTALLED 1 > /dev/null
+echo === Restored configuration for FB06:TR03 ===
