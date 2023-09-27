64a888164b94a17bc3776438eea685eba4edd5ff
Kyle Leleux <kleleux@slac.stanford.edu>
2023-09-27 16:39:41 -0700

    Initial commit of Override.h and Override.cc, which will be used for bypassing certain elements of the code to test BLD.

--- fastFeedbackApp/src/framework/Override.cc
+++ fastFeedbackApp/src/framework/Override.cc
@@ -9,3 +9,11 @@ Override &Override::getInstance() {
     return _instance;
 }
 
+void Override::setOverrideState(bool overrideState) {
+    _overrideState = overrideState;
+}
+
+bool Override::getOverrideState() {
+    return _overrideState;
+}
+
--- fastFeedbackApp/src/framework/Override.h
+++ fastFeedbackApp/src/framework/Override.h
@@ -7,23 +7,39 @@ FF_NAMESPACE_START
 
 class Override {
     public:
-        static TestRec &getInstance();
-   
+        static Override &getInstance();
+        
+        void setOverrideState(bool overrideState);
+        bool getOverrideState();
+
+        void setBcastStates(bool bcastVal);
+        void getBcastStates();
+
+        void setStates();
+        void getStates();
         
     protected:
 
-        TestRec(std::string slotName="TR01", std::string name="T1") :
-            _slotName(slotName),
-            _name(name),
-            _testPv(slotName + " " + name + "TESTREC"),
-            _test() {
-                _testPv.initScanList();
-                std::cout << "Slot Name: " + slotName << std::endl;
-                std::cout << "Name: " + name << std::endl;
-                std::cout << slotName + " " + name + "TESTREC" << std::endl;
+//        TestRec(std::string slotName="TR01", std::string name="T1") :
+//            _slotName(slotName),
+//            _name(name),
+//            _testPv(slotName + " " + name + "TESTREC"),
+//            _test() {
+//                _testPv.initScanList();
+//                std::cout << "Slot Name: " + slotName << std::endl;
+//                std::cout << "Name: " + name << std::endl;
+//                std::cout << slotName + " " + name + "TESTREC" << std::endl;
+//            }
+        
+        Override(bool overrideState=0) :
+            _overrideState(overrideState) {
+                std::cout << "Override State: " + overrideState << std::endl;
             }
         
         static Override _instance;        
+        
+        bool _overrideState;
+        bool _bcastStates;
 
 };
 FF_NAMESPACE_END
