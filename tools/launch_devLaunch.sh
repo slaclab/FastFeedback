33d8a0b2c92e4816594b2c4a9988cebebd8a475c
Kyle Leleux <kleleux@slac.stanford.edu>
2023-10-24 13:41:04 -0700

    added script to launch dev launch screen

--- tools/launch_devLaunch.sh
+++ tools/launch_devLaunch.sh
@@ -1,3 +1,3 @@
 source $TOOLS/script/use_pydm.sh
 export PYDM_DISPLAYS_PATH=$TOOLS/pydm/display/
-pydm --hide-nav-bar -m "LOOP=FB04:TR01" $PYDM/fbck/fbck_details.py & 
+pydm --hide-nav-bar -m "LOOP=FB06:TR01" $PYDM/fbck/fbck_details.py & 
