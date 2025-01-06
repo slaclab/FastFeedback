e544500b57c97f9bdd3b445035a3ce1211e2dead
Kyle Leleux <kleleux@slac.stanford.edu>
2025-01-06 09:34:03 -0800

    Adding a second dev IOC to transmit FCOM data

---
+++ iocBoot/sioc-b34-lg01/st.cmd
@@ -0,0 +1,85 @@
+#!../../bin/linuxRT-x86_64/fastFeedback
+# st.cmd file for Fast Feedback Controller IOC
+# Note: contains parameters specific to sioc-b34-lg01 and
+#       and calls generic st.cmd common to all feedback loops 
+# author: L. Piccoli, A. Babbitt, R. Reno
+##########################################################
+
+# Where am I?
+pwd()
+< envPaths
+
+cd ${TOP} 
+
+#######################################################################
+#Setup environment variables
+#######################################################################
+#=======================================================================
+#Setup some additional environment variables
+#=======================================================================
+
+# tag messages with IOC name
+# How to escape the "sioc-b34-lg01" as the PERL program
+# will try to repplace it.
+# So, uncomment the following and remove the backslash
+
+epicsEnvSet("EPICS_IOC_LOG_CLIENT_INET","${VIOC}")
+
+#======================================================================
+## iocAdmin environment variables
+#=====================================================================
+epicsEnvSet("ENGINEER","J.Mock")
+epicsEnvSet("LOCATION","cpu-sys0-fb01")
+
+#========================================================================
+# Fast Feedback Application Specific Environment Variables
+#========================================================================
+
+#System Location:
+epicsEnvSet("FB", "FB16")
+epicsEnvSet("LOOP", "LG01")
+epicsEnvSet("D", "FBCK:$(FB):$(LOOP)")
+epicsEnvSet("CONFIG_NAME", "LaunchLoop1")
+
+# Which BY1 bend magnet do we want to read energy from?
+epicsEnvSet("BEND_MAG", "BEND:LTUS:525:BDES")
+
+#===================================================================
+# Set MACROS for EVRs & VIOC
+#===================================================================
+# SYS0 ==> LCLS1
+# SYS1 ==> FACET
+
+epicsEnvSet("LOCA","B34")
+epicsEnvSet(FAC,"${LOCA}")
+epicsEnvSet(UNIT,"LG01") 
+epicsEnvSet(EVR_DEV1,"EVR:${FAC}:${UNIT}")
+epicsEnvSet(VEVR, "vevr0")
+
+
+epicsEnvSet("IOC_TYPE", "SIOC")
+epicsEnvSet("IOC_NAME",  "${IOC_TYPE}:${LOCA}:${UNIT}")
+epicsEnvSet("LOCAL_SETPOINTS", "1")
+epicsEnvSet("IOCSH_PS1","epics@${VIOC}>")
+
+# =================================================================
+# User defined environment vars
+# 
+# =================================================================
+
+# END: Additional Environment variables
+#======================================================================
+
+# ====================================================================
+## Load common fast feedback st.cmd
+# ====================================================================
+dbLoadDatabase("dbd/fastFeedback.dbd")
+fastFeedback_registerRecordDeviceDriver(pdbbase)
+
+dbLoadRecords("db/fbckSettled.db",    "LP=FBCK:$(FB):$(LOOP)")
+dbLoadRecords("db/fbckSettledSum.db", "LP=FBCK:$(FB):$(LOOP)")
+dbLoadRecords("db/fbckAutoAct.db",    "LP=FBCK:$(FB):$(LOOP)")
+
+<iocBoot/common/st.cmd
+
+#Done  
