9347008457a6b242a76b31b20ab4f7461759dd5b
Kyle Leleux <kleleux@slac.stanford.edu>
2026-09-09 15:38:03 -0700

    Fixing a problem with the $(I) macro which would cause the ioc to constantly look for a PV called MS since we didn't instantiate $(I) correctly. Fixing for all affected iocs

--- iocBoot/sioc-sys0-lg02/st.cmd
+++ iocBoot/sioc-sys0-lg02/st.cmd
@@ -96,12 +96,12 @@ dbLoadRecords("db/fbckSettled.db",     "LP=FBCK:$(FB):$(LOOP)")
 dbLoadRecords("db/fbckSettledSum.db",  "LP=FBCK:$(FB):$(LOOP)")
 dbLoadRecords("db/fbckAutoActLong.db", "LP=FBCK:$(FB):$(LOOP)")
 
-dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=200,   LO=-90,    AD=5, PR=3, I='', LNK='', ATTR=DL1_ENERGY2,  INP=STATE1, SINK_SIZE=1")
-dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=400,   LO=-150,   AD=5, PR=3, I='', LNK='', ATTR=BC1_ENERGY2,  INP=STATE2, SINK_SIZE=1")
-dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=amps, HO=450,   LO=-300,   AD=5, PR=3, I='', LNK='', ATTR=BC1_CURRENT2, INP=STATE3, SINK_SIZE=1")
-dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=6500,  LO=-1700,  AD=5, PR=3, I='', LNK='', ATTR=BC2_ENERGY2,  INP=STATE4, SINK_SIZE=1")
-dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=amps, HO=50000, LO=-28000, AD=5, PR=3, I='', LNK='', ATTR=BC2_CURRENT2, INP=STATE5, SINK_SIZE=1")
-dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=17400, LO=-1,     AD=5, PR=3, I='', LNK='', ATTR=DL2_ENERGY2,  INP=STATE6, SINK_SIZE=1")
+dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=200,   LO=-90,    AD=5, PR=3, I='FBCK:SYS0:1:DL1_ENERGY2',  LNK='', ATTR=DL1_ENERGY2,  INP=STATE1, SINK_SIZE=1")
+dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=400,   LO=-150,   AD=5, PR=3, I='FBCK:SYS0:1:BC1_ENERGY2',  LNK='', ATTR=BC1_ENERGY2,  INP=STATE2, SINK_SIZE=1")
+dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=amps, HO=450,   LO=-300,   AD=5, PR=3, I='FBCK:SYS0:1:BC1_CURRENT2', LNK='', ATTR=BC1_CURRENT2, INP=STATE3, SINK_SIZE=1")
+dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=6500,  LO=-1700,  AD=5, PR=3, I='FBCK:SYS0:1:BC2_ENERGY2',  LNK='', ATTR=BC2_ENERGY2,  INP=STATE4, SINK_SIZE=1")
+dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=amps, HO=50000, LO=-28000, AD=5, PR=3, I='FBCK:SYS0:1:BC2_CURRENT2', LNK='', ATTR=BC2_CURRENT2, INP=STATE5, SINK_SIZE=1")
+dbLoadRecords("db/bsaFbck.db",  "D=FBCK:SYS0:1, EG=MeV,  HO=17400, LO=-1,     AD=5, PR=3, I='FBCK:SYS0:1:DL2_ENERGY2',  LNK='', ATTR=DL2_ENERGY2,  INP=STATE6, SINK_SIZE=1")
 
 <iocBoot/common/st.cmd
 
