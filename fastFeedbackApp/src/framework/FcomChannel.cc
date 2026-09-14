6b2f188408335e882197b3f5af79bc57d65cf6f8
Kyle Leleux <kleleux@slac.stanford.edu>
2026-09-14 15:32:33 -0700

    Figured out a bug with the creation of FcomChannels where it runs twice. First, it runs for the :GETFCOM_ mux, which we do not want to append 2 to this. Second, it runs to create a writable fcom channel for the actuator itself using a read only CaChannel, which needs the appended 2 to get the correct starting actuator setpoint.

--- fastFeedbackApp/src/framework/FcomChannel.cc
+++ fastFeedbackApp/src/framework/FcomChannel.cc
@@ -98,15 +98,10 @@ int FcomChannel::initialize() {
     if (_accessType == CommunicationChannel::WRITE_ONLY) {
         std::string _caName = _name;
 
-    if (ExecConfiguration::getInstance().getFeedbackType() == 1 && !_caName.empty()) {
-        std::size_t pos = _caName.find(":ADES");
-        if (pos == std::string::npos)
-            pos = _caName.find(":PDES");
-
-        if (pos != std::string::npos && _caName.compare(pos + 5, 1, "2") != 0) {
-            _caName.insert(pos + 5, "2");
+        // This condition is for a non empty SXR actuator device (not :GETFCOM device) that has not already had 2 added to it.
+        if (ExecConfiguration::getInstance().getFeedbackType()==1 && !_caName.empty() && _caName.back() != '2' && _caName.find(":GETFCOM_") == std::string::npos) {
+            _caName += "2";
         }
-    }   
 
         _readCaChannel = new CaChannel(CommunicationChannel::READ_ONLY, _caName);
     }
